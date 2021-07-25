01c994a15cab1dc6f981f1316902cc80c9b0cb7e
kedricdaly <kdaly24@gmail.com>
2021-07-24 22:26:40 -0400

    init commit - first working version

--- 2021/final-project/quickstart.py
+++ 2021/final-project/quickstart.py
@@ -1,9 +1,14 @@
 from __future__ import print_function
 import os.path
+import re
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from timeit import default_timer as timer
+import requests as req
+import time
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -47,24 +52,107 @@ def main():
             print(msg['id'])
     """
 
+    email_dict = {}
+    message_headers = []
+    apiTimer = 0
+    appendTimer = 0
+
     if messages_found:
+        iMsg = 0;
         for msg in msg_list:
-            message = service.users().messages().get(userId='me', id=msg['id'], format='metadata').execute()
+            apiStart = timer()
+            message = service.users().messages().get(userId='me', id=msg['id'], format='metadata', metadataHeaders=["Delivered-To"]).execute()
+            apiEnd = timer()
+            apiTimer += apiEnd - apiStart
             #header = message.get('headers',[])
-            message_header = message.get('headers')
-            print("header:\n")
-            print(message_header)
+            appendStart = timer()
+            # only append messages with a 'Delivered-To' header
+            if 'headers' in message['payload']:
+                message_headers.append(message['payload']['headers'])
+            appendEnd = timer()
+            appendTimer += appendEnd - appendStart
+            #iMsg += 1
+            #if iMsg > 1:
+            #    break
+    
+    iMsg = 0;
 
+    email_list = map(lambda x: x[0]['value'].lower(), message_headers)
+    unique_emails = list(set(email_list))
+    unique_emails.sort()
+    print(f"Identified {len(unique_emails)} unique emails in {len(msg_list)} messages: \n")
+    for email in unique_emails:
+        print(email)
+    
+    print("\n", end="")
 
+    """
+    for msg in message_headers:
+        if iMsg % 79 == 0:
+            print(".", end="")
+            #print("\n")
+        for header in msg:
+            # underlying_dict = header[0]
+            if header['name'] == 'Delivered-To':
+                email_dict[header['value'].lower()] = 1
+                break
+            #elif header['name'] == 'To':
+            # This was only being used for sent messages, and we want to skip sent messages
+            #    email_val = find_between(header['value'],'<','>')
+            #    email_dict[email_val.lower()] = 1
+            #    break
+            else:
+                # unknown delivery address
+                a = 1
+    
+    # print out list of unique addresses (keys in the email_dict dictionary)
+    print("\n")
+    for key in email_dict.keys():
+        print(key)
+    """
 
 
 
 def list_messages(service):
-    results = service.users().messages().list(userId='me').execute()
+    # looping adapted from: https://stackoverflow.com/questions/57733991/get-all-emails-with-google-python-api-client
+    MAX_MESSAGES = 10000
+    query = 'not in:sent' # omit sent messages
+    results = service.users().messages().list(userId='me', maxResults=500, includeSpamTrash=True).execute()
     msg_list = results.get('messages',[])
+
+    nextPageToken = None
+    if "nextPageToken" in results:
+        nextPageToken = results['nextPageToken']
+
+    while nextPageToken:
+        results = service.users().messages().list(userId='me', maxResults=500, includeSpamTrash=True, pageToken=nextPageToken).execute()
+        new_msg_list = results.get('messages',[])
+        msg_list.extend(new_msg_list)
+        if len(msg_list) >= MAX_MESSAGES:
+            break
+        if 'nextPageToken' in results:
+            nextPageToken = results['nextPageToken']
+        else:
+            break
+    #nextPageToken = results.get('nextPageToken')
+    #print("nextPageToken: %s" % nextPageToken)
+    #results2 = service.users().messages().list(userId='me', pageToken=nextPageToken, maxResults=500, includeSpamTrash=True).execute()
+    #msg_list2 = results2.get('messages',[])
+    #thirdPageToken = results2.get('nextPageToken')
+    #print("thirdPageToken: %s" % thirdPageToken)
     #print('msg_list:\n')
+    #msg_list.extend(msg_list2)
+    #print("Len msg_list: %d" % len(msg_list))
     return msg_list
     #print(msg_list)
+    
+
+# https://stackoverflow.com/questions/3368969/find-string-between-two-substrings
+def find_between(s, start, end):
+    try:
+        return (s.split(start))[1].split(end)[0]
+    except:
+        return s
 
 if __name__ == '__main__':
     main()
